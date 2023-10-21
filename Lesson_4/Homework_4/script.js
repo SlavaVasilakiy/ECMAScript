6026fa088e8ea60c96c08d57ade20ba8ad5aa32f
SlavaVasilakiy <slk55slk@gmail.com>
2023-10-21 21:51:12 +0300

    im done

--- Lesson_4/Homework_4/script.js
+++ Lesson_4/Homework_4/script.js
@@ -10,34 +10,66 @@
 
 // Работа должна быть выполнена с API: https://reqres.in/
 
-async function getUserData(ID) {
-	let response = await fetch("https://reqres.in/");
-	if (response.ok) {
-		return await response.json();
-	}
-	throw new Error(`Ошибка запроса`);
+/**
+ * @param {number} id positive number <= 12
+ */
+async function getUserData(id) {
+  const response = await fetch(`https://reqres.in/api/users/${id}`);
+  if (response.ok) {
+    console.log(await response.json());
+  } else {
+    throw new Error(
+      `Ошибка запроса, код ошибки: ${response.status}, запрашиваемый адрес не найден!`
+    );
+  }
 }
 
+getUserData(1);
 
 // Задание 2. Отправка данных на сервер.
 
 // Реализуйте функцию saveUserData, которая принимает объект с данными о пользователе в качестве аргумента и использует fetch для отправки этих данных на удаленный сервер для сохранения. Функция должна возвращать промис, который разрешается, если данные успешно отправлены, или отклоняется в случае ошибки.
 
-// *Подсказка *
+async function saveUserData(userData) {
+  // Формируем объект запроса
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", // Устанавливаем тип содержимого как JSON
+    },
+    body: JSON.stringify(userData), // Преобразуем данные в формат JSON и отправляем в теле запроса
+  };
 
-// // Пример использования функции
-// const user = {
-//   "name": "John Doe",
-//   "job": "unknown"
-// };
+  try {
+    const response = await fetch("https://reqres.in/api/users", requestOptions);
+    if (response.ok) {
+      console.log(response.status);
+      return response.json();
+    } else {
+      throw new Error(`Ошибка запроса, код ошибки: ${response.status}`);
+    }
+  } catch (error) {
+    throw error;
+  }
+}
 
-// saveUserData(user)
-//   .then(() => {
-//     console.log('User data saved successfully');
-//   })
-//   .catch(error => {
-//     console.log(error.message);
-//   });
+async function saveUserDataAndHandleResponse(userData) {
+  try {
+    const response = await saveUserData(userData);
+    console.log("Данные успешно сохранены:", response);
+  } catch (error) {
+    console.error("Ошибка:", error.message);
+  }
+}
+
+// Пример использования функции
+const user = {
+  name: "John Doe",
+  job: "unknown",
+};
+
+saveUserDataAndHandleResponse(user);
+
 // saveUserData использует fetch для отправки данных о пользователе на удаленный сервер для сохранения. Она отправляет POST-запрос на URL-адрес /api/users с указанием типа содержимого application/json и сериализует объект с данными о пользователе в JSON-строку с помощью JSON.stringify(). Если запрос успешен (с кодом 201), функция разрешает промис. Если запрос неуспешен, функция отклоняет промис с сообщением об ошибке.
 
 // Работа должна быть выполнена с API: https://reqres.in/
@@ -46,5 +78,22 @@ async function getUserData(ID) {
 
 // Напишите функцию changeStyleDelayed, которая принимает id элемента и время задержки (в миллисекундах) в качестве аргументов. Функция должна изменить стиль (любой, например - цвет текста) элемента через указанное время.
 
-// // Пример использования функции
-// changeStyleDelayed('myElement', 2000); // Через 2 секунды изменяет стиль элемента с id 'myElement'"
+function changeStyleDelayed(id, time, time2) {
+  setTimeout(() => {
+    if (id) {
+      id.forEach((e) => {
+        e.classList.add("rainbow-text");
+        e.textContent = "А через 5 секунд у тебя кругом пойдёт голова";
+        setTimeout(() => {
+          e.classList.replace("rainbow-text", "rainbow-rotate-text");
+          e.textContent = "88888888888888888888888888888888";
+        }, time2);
+      });
+    }
+  }, time);
+}
+
+const myElement = document.querySelectorAll("#text");
+
+// Пример использования функции
+changeStyleDelayed(myElement, 2000, 5000);
